c928b752566cdcaeaf41a471103c14c7dae618b4
Bazhen-master <lexbazhen@gmail.com>
2025-05-15 00:31:23 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,28 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          50: '#f0f9ff',
+          100: '#e0f2fe',
+          // ... добавьте свою шкалу цветов
+          600: '#0284c7',
+          700: '#0369a1',
+        },
+        wood: {
+          400: '#a87c5b',
+          500: '#8d6b4f',
+        }
+      }
+    }
   },
   plugins: [],
-};
-theme: {
-  extend: {
-    fontFamily: {
-      roboto: ['Roboto', 'sans-serif'],
-    }
-  }
 }
