f7d454047bae86bfa2bb5ccb56f6cb14ff444468
Bazhen-master <lexbazhen@gmail.com>
2025-05-15 00:39:29 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    host: true,
+    port: Number(process.env.PORT) || 3000,
   },
 });
