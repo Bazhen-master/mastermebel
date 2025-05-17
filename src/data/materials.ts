55fcb50e92f9cfb56f4764345fdb22fa718e01a0
Bazhen-master <lexbazhen@gmail.com>
2025-05-17 19:33:15 +0300

    Update materials.ts

--- src/data/materials.ts
+++ src/data/materials.ts
@@ -5,18 +5,18 @@ export const materials: Material[] = [
     id: 'glass',
     name: 'Стекло',
     description: 'Прозрачное, матовое или тонированное стекло для современных и стильных решений.',
-    imageUrl: 'https://images.pexels.com/photos/6969624/pexels-photo-6969624.jpeg',
+    imageUrl: '/images/materials/glass.jpg',
   },
   {
     id: 'mirror',
     name: 'Зеркало',
     description: 'Качественные зеркала для создания визуального простора и функционального дизайна.',
-    imageUrl: 'https://images.pexels.com/photos/1528975/pexels-photo-1528975.jpeg',
+    imageUrl: '/images/materials/mirror.jpg',
   },
   {
     id: 'chipboard',
     name: 'ЛДСП',
     description: 'Ламинированная древесно-стружечная плита различных цветов и текстур для долговечной мебели.',
-    imageUrl: 'https://images.pexels.com/photos/7319316/pexels-photo-7319316.jpeg',
+    imageUrl: '/images/materials/chipboard.jpg',
   },
 ];
